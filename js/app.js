d04d67ca81b28bd3e17a283b0897e8df609f564f
Daz <shoeshinesamurai@gmail.com>
2025-01-20 12:34:24 -0800

    oop dom exercise

--- js/app.js
+++ js/app.js
@@ -1,3 +1,43 @@
 class Calculator {
+  constructor() {
+    this.num1 = parseFloat(document.querySelector('#num1').value);
+    this.num2 = parseFloat(document.querySelector('#num2').value);
+    this.operation = document.querySelector('#operation').value;
+  }
 
+  add() {
+    return this.num1 + this.num2;
+  }
+
+  subtract() {
+    return this.num1 - this.num2;
+  }
+
+  multiply() {
+    return this.num1 * this.num2;
+  }
+
+  divide() {
+    return this.num1 / this.num2;
+  }
+
+  calculate() {
+    switch (this.operation) {
+      case 'add':
+        return this.add();
+      case 'subtract':
+        return this.subtract();
+      case 'multiply':
+        return this.multiply();
+      case 'divide':
+        return this.divide();
+      default:
+        break;
+    }
+  }
 }
+
+document.querySelector('#calculateBtn').addEventListener('click', () => {
+  const result = new Calculator(num1, num2, operation).calculate();
+  document.querySelector('#result').innerHTML = result;
+})
